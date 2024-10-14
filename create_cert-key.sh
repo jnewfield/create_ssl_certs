da907f529ddb796bc327643b807222ed13916300
Jacob Newfield <j.newfield@f5.com>
2024-10-14 14:09:21 -0700

    Changes have been made

--- create_cert-key.sh
+++ create_cert-key.sh
@@ -2,21 +2,25 @@
 # Create a cert and key with openssl
 
 # Variables
-#!/bin/sh
-if [ $1 = "-e" ]; then
-  DAYS_TO_EXPIRE=$2
-else
-  DAYS_TO_EXPIRE=365
-fi
-
-# This script expects a command line argument (name of cert, such as fqdn)
-if [ ! $1 = "-h" ]; then 
-  echo 'Argument required (name of cert & key to be created, such as fqdn)'
-  echo 'Since no argument found using example.com'
-  FQDN=example.com
-else
-  FQDN=$1
-fi
+DAYS_TO_EXPIRE=365
+FQDN=wild.example.com
+# idiomatic parameter and option handling in sh
+while test $# -gt 0
+do
+  case "$1" in
+	-e) DAYS_TO_EXPIRE="$2"
+	    ;;
+  -h) FQDN="$2"
+      ;;
+  -r) rsc="$2"
+      ;;
+  -u) uri="$2"
+      ;;
+	*)
+		;;
+  esac
+  shift
+done
 
 # Remind user to update alt_names in myopenssl.cnf
 read -n 1 -r -s -p "Be sure to edit myopenssl.cnf file and remove or add SANS. Press any key to continue..." key
@@ -34,14 +38,14 @@ rm ./ca/db/index.txt && touch ./ca/db/index.txt
 
 # Create key
 openssl genrsa -out ./certs/$FQDN/$FQDN.key 2048
-#openssl genrsa -des3 -out ./certs/$FQDN/$FQDN.key 2048 -aes-256-cbc
 
 # Create csr
-openssl req -new -config ./myopenssl.cnf -key ./certs/$FQDN/$FQDN.key -out ./ca/reqs/$FQDN.csr
+openssl req -new -config ./myopenssl.cnf -subj "/C=US/ST=Washington/L=Seattle/O=F5 Networks/OU=Profesional Services/CN=$FQDN/emailAddress=j.newfield@f5.com" -key ./certs/$FQDN/$FQDN.key -out ./ca/reqs/$FQDN.csr 
 
 # Sign csr with cnf settings
-openssl ca -config ./myopenssl.cnf -extensions server_cert -days $DAYS_TO_EXPIRE -notext -in ./ca/reqs/$FQDN.csr -out ./certs/$FQDN/$FQDN.crt
+openssl ca -batch -config ./myopenssl.cnf -extensions server_cert -days $DAYS_TO_EXPIRE -notext -in ./ca/reqs/$FQDN.csr -out ./certs/$FQDN/$FQDN.crt
 
+exit 1
 # Add cacert to cert
 cat ./certs/$FQDN/$FQDN.crt | cat - ./ca/certs/APMNetCA.crt > ./certs/$FQDN/$FQDN.withCA.crt
 
