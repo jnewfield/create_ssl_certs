76954805a3a541751475e8ba9034181603a820d5
Jacob Newfield <j.newfield@f5.com>
2024-10-16 18:45:47 -0700

    Changes have been made

--- create_cert-key.sh
+++ create_cert-key.sh
@@ -24,6 +24,8 @@ die () {
 # Variables
 DAYS_TO_EXPIRE=365
 FQDN=wild.example.com
+DIR=$(pwd)
+
 # Extension file for SANS and other things
 echo 'subjectAltName = DNS:test.example.com,DNS:test1.example.com,DNS:test2.example.com,DNS:wild.example.com' > ./ca/extfile.cnf
 # idiomatic parameter and option handling in sh
@@ -50,29 +52,29 @@ done
 
 
 # Remove existing certs, if any
-rm -rf ./certs/$FQDN
+rm -rf $DIR/certs/$FQDN
 
 # Make certs directory
-mkdir -p ./certs/$FQDN
+mkdir -p $DIR/certs/$FQDN
 
 # Reset db
-rm ./ca/db/index.txt && touch ./ca/db/index.txt
+rm $DIR/ca/db/index.txt && touch $DIR/ca/db/index.txt
 
 # Create key
-openssl genrsa -out ./certs/$FQDN/$FQDN.key 2048
+openssl genrsa -out $DIR/certs/$FQDN/$FQDN.key 4096
 
 # Create csr
-openssl req -new -subj "/C=US/ST=Washington/L=Seattle/O=Widgets n Things/OU=doodles/CN=$FQDN/emailAddress=user@example.com" -key ./certs/$FQDN/$FQDN.key -out ./ca/reqs/$FQDN.csr 
+openssl req -new -subj "/C=US/ST=Washington/L=Seattle/O=Widgets n Things/OU=doodles/CN=$FQDN/emailAddress=user@example.com" -key $DIR/certs/$FQDN/$FQDN.key -out $DIR/ca/reqs/$FQDN.csr 
 
 # Sign csr with cnf settings
 #openssl ca -batch -extensions server_cert -days $DAYS_TO_EXPIRE -notext -in ./ca/reqs/$FQDN.csr -out ./certs/$FQDN/$FQDN.crt
-openssl x509 -req -in ./ca/reqs/$FQDN.csr -CA ./ca/certs/exampleCA.crt -CAkey ./ca/certs/exampleCA.key -out ./certs/$FQDN/$FQDN.crt -days $DAYS_TO_EXPIRE -extfile ./ca/extfile.cnf
+openssl x509 -req -in $DIR/ca/reqs/$FQDN.csr -CA $DIR/ca/certs/exampleCA.crt -CAkey $DIR/ca/certs/exampleCA.key -out $DIR/certs/$FQDN/$FQDN.crt -days $DAYS_TO_EXPIRE -extfile $DIR/ca/extfile.cnf
 
 # Add cacert to cert
-cat ./certs/$FQDN/$FQDN.crt | cat - ./ca/certs/exampleCA.crt > ./certs/$FQDN/$FQDN.withCA.crt
+cat $DIR/certs/$FQDN/$FQDN.crt | cat - $DIR/ca/certs/exampleCA.crt > $DIR/certs/$FQDN/$FQDN.withCA.crt
 
 # Create pfx
-openssl pkcs12 -export -out ./certs/$FQDN/$FQDN.withCA.pfx -inkey ./certs/$FQDN/$FQDN.key -in ./certs/$FQDN/$FQDN.withCA.crt -passout pass:
+openssl pkcs12 -export -out $DIR/certs/$FQDN/$FQDN.withCA.pfx -inkey $DIR/certs/$FQDN/$FQDN.key -in $DIR/certs/$FQDN/$FQDN.withCA.crt -passout pass:
 
 # Output certificate
-openssl x509 -in ./certs/$FQDN/$FQDN.crt -text -noout | grep 'Certificate:' -A 10
+openssl x509 -in $DIR/certs/$FQDN/$FQDN.crt -text -noout | grep 'Certificate:' -A 10
---
+++ nim_certs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Create certs and upload to NIM
+
+# Variables
+NIM=nms.es.f5net.com
+START_ITR=1
+END_ITR=105
+DIR=$(pwd)
+CREDS=$(echo -n admin:admin | base64)
+SYSTEM_UID=704253e9-233c-3277-bbc7-a343a3be34e2
+INSTANCE=9ec4ec22-5463-56c6-9d26-ee27d815ff07
+DELETE_CERTS="false"
+
+# idiomatic parameter and option handling in sh
+while test $# -gt 0
+do
+  case "$1" in
+  -d | --delete) DELETE_CERTS="true"
+    ;;
+  *)
+    ;;
+  esac
+  shift
+done
+for i in $(seq $START_ITR $END_ITR)
+do 
+  $DIR/create_cert-key.sh --expiry $i --fqdn test$i.example.com 2> /dev/null > /dev/null
+  CERT=$(awk '$1=$1' ORS='\\n' $DIR/certs/test$i.example.com/test$i.example.com.crt)
+  KEY=$(awk '$1=$1' ORS='\\n' $DIR/certs/test$i.example.com/test$i.example.com.key)
+  CACERT=$(awk '$1=$1' ORS='\\n' $DIR/ca/certs/exampleCA.crt)
+  if [ $DELETE_CERTS = "false" ]; then
+    #curl -kL -X POST https://$NIM/api/platform/v1/certs -H "Authorization: Basic $CREDS" -H "Accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"test$i\", \"certAssignmentDetails\": [ { \"assignedKeyPaths\": [ { \"cert\": \"/etc/nginx/ssl/example.com/test$i.crt\", \"key\": \"/etc/nginx/ssl/example.com/test$i.key\" } ], \"configKeyPaths\": [], \"systemUID\": \"$SYSTEM_UID\", \"type\": \"Instance\" } ], \"certPEMDetails\": { \"privateKey\": \"$KEY\", \"publicCert\": \"$CERT\", \"type\": \"PEM\",  \"password\": \"$KEY_PASS\",  \"caCerts\": [\"$CACERT\"] }, \"instanceRefs\": [\"/api/platform/v1/systems/$SYSTEM_UID/instances/$INSTANCE\"] }" 2> /dev/null | jq
+    curl -kL -X POST https://$NIM/api/platform/v1/certs -H "Authorization: Basic $CREDS" -H "Accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"test$i\", \"certAssignmentDetails\": [ { \"assignedKeyPaths\": [ { \"cert\": \"/etc/nginx/ssl/example.com/test$i.crt\", \"key\": \"/etc/nginx/ssl/example.com/test$i.key\" } ], \"configKeyPaths\": [], \"systemUID\": \"$SYSTEM_UID\", \"type\": \"Instance\" } ], \"certPEMDetails\": { \"privateKey\": \"$KEY\", \"publicCert\": \"$CERT\", \"type\": \"PEM\",  \"password\": \"$KEY_PASS\",  \"caCerts\": [\"$CACERT\"] } }" 2> /dev/null | jq
+  else
+      curl -kL -X DELETE https://$NIM/api/platform/v1/certs/test$i -H "Authorization: Basic $CREDS" -H "Accept: application/json" -H "Content-Type: application/json" -v 2>&1 | egrep "HTTP/2|DELETE" | egrep -v "\[|--|using"
+  fi
+  rm -rf $DIR/certs/test$i.example.com > /dev/null
+  sleep 0.1
+done
+
+
